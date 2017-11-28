7bd4a8b37ef4bf670049513ae3c37a9f33d14ee2
程挥竣 <chenghuijun@shhxzq.com>
2017-11-28 10:43:46 +0800

    导入新版样式写法

--- src/Toast.js
+++ src/Toast.js
@@ -1,152 +1,147 @@
-import styles from './style/index.less'
-import React, { PropTypes, Component } from 'react'
-import ReactDOM from 'react-dom'
-import classnames from 'classnames'
+import styles from "./style/index.less";
+import React, { PropTypes, Component } from "react";
+import ReactDOM from "react-dom";
+import classnames from "classnames";
+const defaultPrefixCls = "cefc-toast";
 
 class Message extends Component {
-
-    constructor(props) {
-        super();
-        this.state = {
-            open: false
-        };
-        this.prepareClose(props);
-    }
-
-    componentDidMount() {
-        this.handleOpen();
-        window.addEventListener('scroll', this.handleTouchEvent);
-        // window.addEventListener('click', this.handleTouchEvent);
-    }
-
-    componentWillReceiveProps(nextProps) {
-        'open' in nextProps && this.setState({open: nextProps.open})  //// in 可以用用来判断 json 里面有没有某个属性
-        this.prepareClose(nextProps)
-    }
-
-    componentWillUnmount () {
-        window.removeEventListener('scroll', this.handleTouchEvent);
-        // window.removeEventListener('click', this.handleScroll);
-    }
-
-    prepareClose(props) {
-        const { duration } = props;
-        duration && setTimeout(this.handleClose, duration * 1000)
-    }
-
-    handleTouchEvent = () => {
-        let { open } = this.state;
-
-        if (open) {
-            this.setState({open: false});
-            this.props.onClose && this.props.onClose();
-        }
+  constructor(props) {
+    super();
+    this.state = {
+      open: false
     };
+    this.prepareClose(props);
+  }
 
-    handleClose = () => {
-        this.setState({open: false});
-        this.props.onClose && this.props.onClose();
+  componentDidMount() {
+    this.handleOpen();
+    window.addEventListener("scroll", this.handleTouchEvent);
+    // window.addEventListener('click', this.handleTouchEvent);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    "open" in nextProps && this.setState({ open: nextProps.open }); //// in 可以用用来判断 json 里面有没有某个属性
+    this.prepareClose(nextProps);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleTouchEvent);
+    // window.removeEventListener('click', this.handleScroll);
+  }
+
+  prepareClose(props) {
+    const { duration } = props;
+    duration && setTimeout(this.handleClose, duration * 1000);
+  }
+
+  handleTouchEvent = () => {
+    let { open } = this.state;
+
+    if (open) {
+      this.setState({ open: false });
+      this.props.onClose && this.props.onClose();
     }
+  };
 
-    handleOpen = () => {
-        this.setState({open: true});
-    }
+  handleClose = () => {
+    this.setState({ open: false });
+    this.props.onClose && this.props.onClose();
+  };
 
-    render() {
-        const { type, message, duration } = this.props;
-        const { open } = this.state;
-        let opacity = open ? '0.5' : '0';
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
 
-        return (
-            <div className={classnames('bfd-message', {[`bfd-message--${type}`]: type})} style={{'opacity': opacity}}>
-                {message}
-                {duration === 0 && (
-                    <button className="bfd-message__remove"  onClick={this.handleClose}>
-                      Show Toast
-                    </button>
-                )}
-            </div>
-        )
-    }
+  render() {
+    const { type, message, duration } = this.props;
+    const { open } = this.state;
+    let opacity = open ? "0.5" : "0";
+
+    return <div className={classnames(defaultPrefixCls, {
+          [`${defaultPrefixCls}--${type}`]: type
+        })} style={{ opacity: opacity }}>
+        {message}
+        {duration === 0 && <button className={`${defaultPrefixCls}__remove`} onClick={this.handleClose}>
+            Show Toast
+          </button>}
+      </div>;
+  }
 }
 
 Message.propTypes = {
-    type: PropTypes.oneOf(['success', 'danger']).isRequired,
-    message: PropTypes.oneOfType([
-        PropTypes.node,
-        PropTypes.string,
-    ]).isRequired,
-    duration: PropTypes.number,
-    onClose: PropTypes.func,
-    open: PropTypes.bool
-}
+  type: PropTypes.oneOf(["success", "danger"]).isRequired,
+  message: PropTypes.oneOfType([PropTypes.node, PropTypes.string]).isRequired,
+  duration: PropTypes.number,
+  onClose: PropTypes.func,
+  open: PropTypes.bool,
+  prefixCls: PropTypes.string
+};
 
 let render = props => {
-    const container = document.createElement('div');
-    document.body.appendChild(container);
+  const container = document.createElement("div");
+  document.body.appendChild(container);
 
-    let isOpen = false;
-    const messageQueue = [];
-    const handleClose = () => {
-        isOpen = false;
-        const head = messageQueue[0] && messageQueue.shift();
-        head && render(head);
-    }
+  let isOpen = false;
+  const messageQueue = [];
+  const handleClose = () => {
+    isOpen = false;
+    const head = messageQueue[0] && messageQueue.shift();
+    head && render(head);
+  };
 
-    render = nextProps => {
-        props = Object.assign({}, props, nextProps);
-        if (isOpen && props.open) {
-            return messageQueue.push(props);
-        }
-        isOpen = props.open;
-        ReactDOM.render(<Message {...props} onClose={handleClose} />, container);
-        props.open || handleClose()
+  render = nextProps => {
+    props = Object.assign({}, props, nextProps);
+    if (isOpen && props.open) {
+      return messageQueue.push(props);
     }
-    render()
-}
+    isOpen = props.open;
+    ReactDOM.render(<Message {...props} onClose={handleClose} />, container);
+    props.open || handleClose();
+  };
+  render();
+};
 
 const message = {
+  /**
+   * @public
+   * @name message.success
+   * @param  {string | element} message message 内容，支持 React 元素
+   * @param  {number} [duration] 持续时间，单位秒，为0时手动关闭
+   * @description 成功信息，默认 2 秒后自动关闭
+   */
+  success(message, duration = 2) {
+    render({
+      message,
+      duration,
+      type: "success",
+      open: true
+    });
+  },
 
-    /**
-     * @public
-     * @name message.success
-     * @param  {string | element} message message 内容，支持 React 元素
-     * @param  {number} [duration] 持续时间，单位秒，为0时手动关闭
-     * @description 成功信息，默认 2 秒后自动关闭
-     */
-    success(message, duration = 2) {
-        render({
-            message,
-            duration,
-            type: 'success',
-            open: true
-        })
-    },
+  /**
+   * @public
+   * @name message.danger
+   * @param  {string | element} message message 内容，支持 React 元素
+   * @param  {number} [duration] 持续时间，单位秒，为0时手动关闭
+   * @description 失败信息，默认 3 秒后自动关闭
+   */
+  danger(message, duration = 3) {
+    render({
+      message,
+      duration,
+      type: "danger",
+      open: true
+    });
+  },
 
-    /**
-     * @public
-     * @name message.danger
-     * @param  {string | element} message message 内容，支持 React 元素
-     * @param  {number} [duration] 持续时间，单位秒，为0时手动关闭
-     * @description 失败信息，默认 3 秒后自动关闭
-     */
-    danger(message, duration = 3) {
-        render({
-            message,
-            duration,
-            type: 'danger',
-            open: true
-        })
-    },
+  /**
+   * @public
+   * @name message.close
+   * @description 关闭当前 message
+   */
+  close() {
+    render({ open: false });
+  }
+};
 
-    /**
-     * @public
-     * @name message.close
-     * @description 关闭当前 message
-     */
-    close() {
-        render({open: false})
-    }
-}
-
-export default message
+export default message;
